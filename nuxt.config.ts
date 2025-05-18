a2ea80130a65b01439d1e74f297a418c399d460b
Etienne <hello@etiennemoureton.fr>
2025-05-18 11:02:03 +0200

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  vite: { plugins: [tailwindcss(),], },
+  css: ['~/assets/styles/global.pcss'],
+})
