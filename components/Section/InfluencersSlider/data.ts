ce2f1b290d44462059c15daa583bf455588b75c1
Etienne <hello@etiennemoureton.fr>
2025-11-28 11:08:53 +0100

    feat: remove yao from influencers

--- components/Section/InfluencersSlider/data.ts
+++ components/Section/InfluencersSlider/data.ts
@@ -9,7 +9,6 @@ type Slide = {
 const slidesData: Slide[] = [
   { id: 1, content: 'Slide 1', imageUrl: '/images/talents/polynette-1.png', backgroundUrl: '/images/talents/polynette-2.png', name: 'Polynette' },
   { id: 1, content: 'Slide 1', imageUrl: '/images/talents/fana-1.png', backgroundUrl: '/images/talents/fana-2.png', name: 'Fana' },
-  { id: 1, content: 'Slide 1', imageUrl: '/images/talents/yao-1.jpg', backgroundUrl: '/images/talents/yao-2.jpg', name: 'Yao_ag' },
   { id: 1, content: 'Slide 1', imageUrl: '/images/talents/marwan-1.jpg', backgroundUrl: '/images/talents/marwan-2.jpg', name: 'MarwanMC9' },
   { id: 1, content: 'Slide 1', imageUrl: '/images/talents/lola-1.jpg', backgroundUrl: '/images/talents/lola-2.jpg', name: `Lola 'Evileye'` },
 ];
